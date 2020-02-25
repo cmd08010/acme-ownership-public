8c1f11cb3c25565ecc034f9a9461c6860c6e292c
Ryan Hall <ryanhall999@gmail.com>
2020-02-24 19:53:37 -0500

    added delete

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
-const pg = require("pg")
-const uuid = require("uuid/v4")
+const pg = require("pg");
+const uuid = require("uuid/v4");
 const client = new pg.Client(
-  process.env.DATABASE_URL || "postgres://localhost/acme_db"
-)
+	process.env.DATABASE_URL || "postgres://localhost/acme_db"
+);
 
-client.connect()
+client.connect();
 
 const sync = async () => {
-  //create tables and seed some data
+	//create tables and seed some data
 
-  const SQL = `
+	const SQL = `
   CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
   DROP TABLE IF EXISTS user_things;
   DROP TABLE IF EXISTS users;
@@ -32,39 +32,57 @@ const sync = async () => {
     thingId UUID REFERENCES things(id)
   );
 
-  `
+  `;
 
-  await client.query(SQL)
-}
+	await client.query(SQL);
+};
 
 //Additional methods here for reading, creating, destroying
 
-const getUsers = () => {}
+const getUsers = async () => {
+	const SQL = `SELECT * FROM users returning *`;
+	const response = await client.query(SQL);
+	return response.rows;
+};
 
 const createUser = async name => {
-  const SQL = `INSERT INTO users(name) VALUES ($1)
+	const SQL = `INSERT INTO users(name) VALUES ($1)
   returning *;
-  `
-  const response = await client.query(SQL, [name])
-  return response.rows[0]
-}
+  `;
+	const response = await client.query(SQL, [name]);
+	return response.rows[0];
+};
 
-const destroyUser = () => {}
+const destroyUser = async id => {
+	const SQL = `DELETE FROM users WHERE id = $1;`;
+	await client.query(SQL, [id]);
+};
 
-const getThings = () => {}
+const getThings = async () => {
+	const SQL = `SELECT * FROM things RETURNING *`;
+	const response = await client.query(SQL);
+	return response.rows;
+};
 
 const createThing = async name => {
-  const SQL = `INSERT INTO things(name) VALUES ($1)
+	const SQL = `INSERT INTO things(name) VALUES ($1)
 returning *;
-`
-  const response = await client.query(SQL, [name])
-  return response.rows[0]
-}
+`;
+	const response = await client.query(SQL, [name]);
+	return response.rows[0];
+};
 
-const destroyThing = () => {}
+const destroyThing = async id => {
+	const SQL = `DELETE FROM things WHERE id = $1;`;
+	await client.query(SQL, [id]);
+};
 
 module.exports = {
-  sync,
-  createUser,
-  createThing
-}
+	sync,
+	createUser,
+	createThing,
+	getUsers,
+	getThings,
+	destroyThing,
+	destroyUser
+};
--- server.js
+++ server.js
@@ -1,61 +1,68 @@
-const express = require("express")
-const app = express()
-const path = require("path")
-const db = require("./db")
-const morgan = require("morgan")
-const bodyParser = require("body-parser")
+const express = require("express");
+const app = express();
+const path = require("path");
+const db = require("./db");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
+app.use(
+	morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "index.html"))
-})
+	res.sendFile(path.join(__dirname, "index.html"));
+});
 
 //Additional routes here
 //GET, POST, DELETE for /api/users
 
 app.get("/api/users", (req, res, next) => {
-  console.log("wow")
-})
+	db.getUsers().then(response => res.send(response));
+});
 
 app.post("/api/users", (req, res, next) => {
-  db.createUser(req.body.name).then(response => res.send(response))
-})
-app.delete("/api/users", (req, res, next) => {})
+	db.createUser(req.body.name).then(response => res.send(response));
+});
+
+app.delete("/api/users/:id", (req, res, next) => {
+	console.log(req.params.id);
+	db.destroyUser(req.param.id).then(() => res.sendStatus(200));
+});
 
 //GET, POST, DELETE for /api/things
 app.get("/api/things", (req, res, next) => {
-  console.log(req.body)
-  db.createThing(req.body.name).then(response => res.send(response))
-})
+	db.getThings().then(response => res.send(response));
+});
 
-app.post("/api/things", (req, res, next) => {})
-app.delete("/api/things", (req, res, next) => {})
+app.post("/api/things", (req, res, next) => {
+	db.createThing(req.body.name).then(response => res.send(response));
+});
+app.delete("/api/things", (req, res, next) => {});
 //GET, POST, DELETE for /api/user_things
-app.get("/api/user_things", (req, res, next) => {})
+app.get("/api/user_things", (req, res, next) => {});
 
-app.post("/api/user_things", (req, res, next) => {})
-app.delete("/api/user_things", (req, res, next) => {})
+app.post("/api/user_things", (req, res, next) => {});
+app.delete("/api/user_things", (req, res, next) => {});
 
 app.use((req, res, next) => {
-  next({
-    status: 404,
-    message: `Page not found for ${req.method} ${req.url}`
-  })
-})
+	next({
+		status: 404,
+		message: `Page not found for ${req.method} ${req.url}`
+	});
+});
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500).send({
-    message: err.message || JSON.stringify(err)
-  })
-})
+	res.status(err.status || 500).send({
+		message: err.message || JSON.stringify(err)
+	});
+});
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 db.sync().then(() => {
-  app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-  })
-})
+	app.listen(port, () => {
+		console.log(`listening on port ${port}`);
+	});
+});
